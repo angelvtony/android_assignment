38d2ea12a659ccf540be796366530c3adb62579d
Angel <angelvtony2001@gmail.com>
2023-10-10 17:33:35 +0530

    Login UI completed

--- app/src/main/java/com/acabes/loginscreen/MainActivity.kt
+++ app/src/main/java/com/acabes/loginscreen/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.acabes.loginscreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    val user = "Admin"
+    val pass = "Admin"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userName = findViewById<EditText>(R.id.userName)
+        val password = findViewById<EditText>(R.id.password)
+        val login = findViewById<Button>(R.id.login)
+        login.setOnClickListener{
+            val enteredUser = userName.text.toString()
+            val enteredPass = password.text.toString()
+            if (enteredUser == user && enteredPass == pass){
+
+                val intent = Intent(this, Home::class.java)
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
