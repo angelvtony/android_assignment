d2863b455257c3edfb28732f2fd57f329a29e7c8
Angel <angelvtony2001@gmail.com>
2023-10-13 17:32:45 +0530

    Fragment Activity added

--- app/src/main/java/com/acabes/loginscreen/Home.kt
+++ app/src/main/java/com/acabes/loginscreen/Home.kt
@@ -2,18 +2,40 @@ package com.acabes.loginscreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-
+import androidx.fragment.app.Fragment
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        val greetingMessageTextView = findViewById<TextView>(R.id.greetingMessage)
 
+        val greetingMessageTextView = findViewById<TextView>(R.id.greetingMessage)
+        val master =findViewById<Button>(R.id.master)
+        val visa =findViewById<Button>(R.id.visa)
+        val visaCard = VisaCard()
+        val fragmentCard = CardFragment()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frameFragment, visaCard)
+            commit()
+        }
+
+
+        master.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.frameFragment, visaCard)
+                commit()
+            }
+        }
+        visa.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.frameFragment, fragmentCard)
+                commit()
+            }
+        }
 
         val username = intent.getStringExtra("username")
-
-
         greetingMessageTextView.text = "Hi, $username"
     }
 }
